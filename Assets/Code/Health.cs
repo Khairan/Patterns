b5f8e780b929f3edcfcd431557a6a7b61cde5e5a
Khairan <alphakhairan@gmail.com>
2021-10-11 18:15:59 +0300

    Project add

---
+++ Assets/Code/Health.cs
@@ -0,0 +1,28 @@
+﻿namespace Asteroids
+{
+    public sealed class Health
+    {
+        #region Fields
+
+        public float Max { get; }
+        public float Current { get; private set; }
+
+        #endregion
+
+
+        #region Methods
+
+        public Health(float max, float current)
+        {
+            Max = max;
+            Current = current;
+        }
+
+        public void ChangeCurrentHealth(float hp)
+        {
+            Current = hp;
+        }
+
+        #endregion
+    }
+}
