b4553e04d1a4f0ce270b32453ddef8d00dcaaf61
Khairan <alphakhairan@gmail.com>
2021-10-21 20:14:04 +0300

    Project add

---
+++ Assets/Code/Adapter/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+
+namespace Asteroids.Adapter
+{
+    internal sealed class Enemy : IFire
+    {
+        private readonly IAttack _attack = new AttackBullet();
+
+        public void Fire(Vector3 position)
+        {
+            _attack.Attack(position);
+        }
+    }
+}
---
+++ Assets/Code/RotationShip.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+
+namespace Asteroids
+{
+    internal sealed class RotationShip : IRotation
+    {
+        #region Fields
+
+        private readonly Transform _transform;
+
+        #endregion
+
+        
+        #region Methods
+
+        public RotationShip(Transform transform)
+        {
+            _transform = transform;
+        }
+        
+        public void Rotation(Vector3 direction)
+        {
+            var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            _transform.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+
+        #endregion
+    }
+}
