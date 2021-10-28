e40dc028ed4ab0e20bb8d81300c467a76e9be09c
Khairan <alphakhairan@gmail.com>
2021-10-28 20:27:42 +0300

    Lesson8

---
+++ Assets/ViewModel/IHpViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MVVM.Model;
+
+
+namespace MVVM.ViewModel
+{
+    public interface IHpViewModel
+    {
+        IHpModel HpModel { get; }
+        bool IsDead { get; }
+        void ApplyDamage(float damage);
+        event Action<float> OnHpChange;
+    }
+}
